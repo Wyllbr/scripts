545df037c347a20ccba64970bf6bfcbbb2177a92
w- <wllrntwrk2gmail.com>
2025-03-12 18:57:39 -0400

    upload

---
+++ task2/conta_linhas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo -e "Forneca um arquivo..."
+echo ">"
+read arquivo
+
+if [[ -f "$arquivo" ]]; then
+  # echo "O arquivo é um arquivo"
+   linhas=$(wc -l backup.sh | awk '{print $1}') 
+  # awk '{print $1}' $linhas
+   echo "O número de linhas do arquivo $arquivo é $linhas"
+else
+	echo "não é um arquivo"
+fi
